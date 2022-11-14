c9e47dce30823b199100dd79875d3741ecaee523
L1feXacker <51370356+L1feXacker@users.noreply.github.com>
2022-11-14 19:22:31 +0000

    Deploying to gh-pages from @ L1feXacker/stas.dogada.org@45c07dca0d10139fc321c5f5e26902e1f738d478 🚀

--- _next/static/-bzAKCwDh23KtypLVgowl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-55604545ec50416d.js"],"/RNDPrivacy":["static/chunks/pages/RNDPrivacy-eedcc37a446f6423.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/RNDPrivacy","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/EtLnR8cPyxLxKM0BmNPWa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-55604545ec50416d.js"],"/RNDPrivacy":["static/chunks/pages/RNDPrivacy-c6ea742c99bb5ce4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/RNDPrivacy","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
