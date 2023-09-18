0eac39ca1c286bf92fbd60c09a89d301038b7f45
L1feXacker <51370356+L1feXacker@users.noreply.github.com>
2023-09-18 09:51:55 +0000

    Deploying to gh-pages from @ L1feXacker/stas.dogada.org@87b929f3de90b7f9fc54ab24a6d08648356e7471 🚀

--- _next/static/-tFhLvCLDa90erQwa-4Ro/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8e7c20e71e1c032d.js"],"/RNDPrivacy":["static/chunks/pages/RNDPrivacy-c6ea742c99bb5ce4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/RNDPrivacy","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
